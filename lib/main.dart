801a09ee427a72ad46457d1914a341830661bffe
Kim-AYoung <onionlily123@naver.com>
2021-10-10 22:53:00 +0900

    add login & register & mypage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'register.dart';
+import 'mypage.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,105 +12,103 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Crowd Sourcing',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Spacer(
+              flex: 10,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            // logo
+            Image.network(
+              "https://mblogthumb-phinf.pstatic.net/MjAyMDA5MjdfMjky/MDAxNjAxMjEwNzA4MzU2.fsdz9sAALwhaR1ot0emkfk5SxkthtfFhiQVfaUCVVewg.ndi0Hk1FqHllVImNaXvmNH7NmWvKZV0lVd9H4vc9M8gg.PNG.xuni1021/SE-777d059f-62e3-4d55-9906-529c3c556c80.png?type=w800",
+              width: 200,
+              height: 100,
+            ),
+            Spacer(
+              flex: 7,
+            ),
+            // id
+            SizedBox(
+              width: 300,
+              child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '아이디',
+                  )),
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            // password
+            SizedBox(
+              width: 300,
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '비밀번호',
+                ),
+              ),
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            // register, login button
+            SizedBox(
+              width: 300,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SelfCertificationPage()));
+                    },
+                    child: Text("회원가입"),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyPage()));
+                    },
+                    child: Text('로그인'),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(
+              flex: 20,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/register.dart
@@ -0,0 +1,515 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'mypage.dart';
+
+class SelfCertificationPage extends StatefulWidget {
+  SelfCertificationPage({Key? key}) : super(key: key);
+
+  @override
+  _SelfCertificationPageState createState() => _SelfCertificationPageState();
+}
+
+class _SelfCertificationPageState extends State<SelfCertificationPage> {
+  @override
+  Widget build(BuildContext context) {
+    String phoneNumber;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Spacer(
+              flex: 3,
+            ),
+            // status bar
+            Row(
+              children: [
+                SizedBox(
+                  width: 86,
+                ),
+                Text("본인인증"),
+              ],
+            ),
+            LinearPercentIndicator(
+              alignment: MainAxisAlignment.center,
+              width: 300.0,
+              lineHeight: 15.0,
+              percent: 0.33,
+              progressColor: Colors.green,
+            ),
+            Spacer(
+              flex: 3,
+            ),
+            // phone number
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 200,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: "핸드폰 번호"),
+                    keyboardType: TextInputType.phone,
+                    onChanged: (value) => phoneNumber = value,
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                SizedBox(
+                  width: 80,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Respond to button press
+                    },
+                    child: Text('인증'),
+                  ),
+                )
+              ],
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            // phone authentication number
+            SizedBox(
+              width: 300,
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: "인증 번호"),
+                keyboardType: TextInputType.number,
+                onChanged: (value) => phoneNumber = value,
+              ),
+            ),
+            Spacer(
+              flex: 3,
+            ),
+            // name
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                    width: 100,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: '이름',
+                      ),
+                    )),
+                SizedBox(
+                  width: 200,
+                )
+              ],
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            // Resident Registration Number
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                    width: 140,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: '주민번호',
+                      ),
+                      keyboardType: TextInputType.number,
+                    )),
+                SizedBox(
+                  width: 5,
+                ),
+                Container(
+                  height: 2,
+                  width: 10,
+                  decoration: BoxDecoration(color: Colors.grey),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                SizedBox(
+                    width: 140,
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ))
+              ],
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            // authentication button
+            ElevatedButton(
+              onPressed: () {
+                // Respond to button press
+              },
+              child: Text('실명 인증'),
+            ),
+            Spacer(
+              flex: 3,
+            ),
+            // prev, next button
+            SizedBox(
+              width: 300,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      elevation: 15.0,
+                    ),
+                    child: Text('이전'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CreationAccountPage()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      elevation: 15.0,
+                    ),
+                    child: Text('다음'),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(
+              flex: 3,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CreationAccountPage extends StatefulWidget {
+  CreationAccountPage({Key? key}) : super(key: key);
+
+  @override
+  _CreationAccountPageState createState() => _CreationAccountPageState();
+}
+
+class _CreationAccountPageState extends State<CreationAccountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Spacer(
+              flex: 3,
+            ),
+            // status bar
+            Row(
+              children: [
+                SizedBox(
+                  width: 186,
+                ),
+                Text("계정생성"),
+              ],
+            ),
+            LinearPercentIndicator(
+              alignment: MainAxisAlignment.center,
+              width: 300.0,
+              lineHeight: 15.0,
+              percent: 0.66,
+              progressColor: Colors.green,
+            ),
+            Spacer(
+              flex: 3,
+            ),
+            // id
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 190,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: "아이디"),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                SizedBox(
+                  width: 90,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Respond to button press
+                    },
+                    child: Text('중복 확인'),
+                  ),
+                )
+              ],
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            // password
+            SizedBox(
+              width: 300,
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '비밀번호',
+                ),
+              ),
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            // password re-confirmation
+            SizedBox(
+              width: 300,
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '비밀번호 확인',
+                ),
+              ),
+            ),
+            Spacer(
+              flex: 2,
+            ),
+            // prev, next button
+            SizedBox(
+              width: 300,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      elevation: 15.0,
+                    ),
+                    child: Text('이전'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => OtherInformationPage()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      elevation: 15.0,
+                    ),
+                    child: Text('다음'),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(
+              flex: 10,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OtherInformationPage extends StatefulWidget {
+  OtherInformationPage({Key? key}) : super(key: key);
+
+  @override
+  _OtherInformationPageState createState() => _OtherInformationPageState();
+}
+
+class _OtherInformationPageState extends State<OtherInformationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Spacer(flex: 3,),
+            // status bar
+            Row(
+              children: [
+                SizedBox(
+                  width: 286,
+                ),
+                Text("기타정보"),
+              ],
+            ),
+            LinearPercentIndicator(
+              alignment: MainAxisAlignment.center,
+              width: 300.0,
+              lineHeight: 15.0,
+              percent: 1,
+              progressColor: Colors.green,
+            ),
+            Spacer(flex: 2,),
+            // sub title
+            SizedBox(
+              width: 300,
+              child: Text(
+                "차량정보",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+            Spacer(flex: 1,),
+            // car info
+            SizedBox(
+              width: 300,
+              child: TextField(
+                  decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '차량정보 (예시: 모닝)',
+              )),
+            ),
+            Spacer(flex: 1,),
+            // car number
+            SizedBox(
+              width: 300,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '차량번호 (예시 : 11가 1234)',
+                ),
+              ),
+            ),
+            Spacer(flex: 1,),
+            // car license plate color
+            SizedBox(
+              width: 300,
+              child: Row(
+                children: [
+                  Text("차량 번호판 색상"),
+                  SizedBox(width: 20,),
+                  DropdownButton(
+                    value: '흰색',
+                    items: ['흰색', '노란색'].map(
+                      (value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Text(value),
+                        );
+                      },
+                    ).toList(),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(flex: 2,),
+            // sub title
+            SizedBox(
+              width: 300,
+              child: Text(
+                "정산정보",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+            Spacer(flex: 1,),
+            // bank
+            SizedBox(
+              width: 300,
+              child: Row(
+                children: [
+                  Text("은행"),
+                  SizedBox(width: 20,),
+                  DropdownButton(
+                    value: '카카오뱅크',
+                    items: ['카카오뱅크', '케이뱅크'].map(
+                          (value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Text(value),
+                        );
+                      },
+                    ).toList(),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(flex: 1,),
+            // account number
+            SizedBox(
+              width: 300,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '계좌번호',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Spacer(flex: 2,),
+            // prev, next button
+            SizedBox(
+              width: 300,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      elevation: 15.0,
+                    ),
+                    child: Text('이전'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyPage()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      elevation: 15.0,
+                    ),
+                    child: Text('완료'),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(flex: 2,),
+          ],
+        ),
+      ),
+    );
+  }
+}
